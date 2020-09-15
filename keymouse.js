1a907536344b75e5c6a8cab09eecb763972ddaae
Rory Cawley <rory.cawley@gmail.com>
2020-09-15 21:50:57 +0100

    updates

--- keymouse.js
+++ keymouse.js
@@ -7,10 +7,17 @@ const puppeteer = require('puppeteer')
   })
 
   const page = await browser.newPage()
-
+  await page.setViewport({
+    width: 1440,
+    height: 1000
+  })
+  // login
   await page.goto('https://demosite.executeautomation.com/')
   await page.type('[name=UserName', 'executeautomation')
-  await page.type('[name=Password', 'executeautomation')
+  await page.type('[name=Password', 'admin')
   await page.keyboard.press('Enter', { delay: 2000 })
+
+  // hover over menu
+  await page.hover("[id='Automation Tools']")
   await browser.close()
 })()
